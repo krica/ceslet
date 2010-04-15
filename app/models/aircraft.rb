2d17f60e22748fbe6582a41729e02ac2292af99c
Jan Křička <krica@gravastar.cz>
2010-04-15 19:21:26 +0200

    Prepared Aircraft for company

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -3,4 +3,22 @@ class Aircraft < ActiveRecord::Base
 
   belongs_to :aircraft_model
 
+
+  #returns operator company name if present
+  def operator_name
+    if self.operator_id && @operator = Company.find(self.operator_id)
+      return @operator.name
+    else
+      return ""
+    end
+  end
+
+  #returns owner company name if present
+  def owner_name
+    if self.owner_id && @owner = Company.find(self.owner_id)
+      return @owner.name
+    else
+      return ""
+    end
+  end
 end
